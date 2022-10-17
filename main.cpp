69cf42f6f9099166aae5cd46d72a2dc146e999a9
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-17 09:30:51 +0900

    課題提出時点

--- main.cpp
+++ main.cpp
@@ -1,20 +1,50 @@
 #include <stdio.h>
 
-//再帰関数で階乗を出力
-int Recursive(int n) {
-	if (n <= 1) {
-		return (1);
+//再帰関数で、再帰的な体制の合計賃金を計算
+///<int n == 再帰させる値>
+///<int remain == 残りカウント>
+int Recursive(int n, int remain) {
+	//残りカウントを減らす
+	remain--;
+
+	//途中結果をプリントしておく
+	printf("現賃金 = %d,残り = %d\n",
+		n,remain);
+
+	//残りのカウントが0なら終了
+	if (remain <= 0) {
+		printf("\n");
+
+		//最終出力
+		return n;
 	}
-	return (n * Recursive(n - 1));
+
+	//前回の値を2倍し、50引いた値を、次の引数とする
+	return Recursive((2*n)-50,remain);
 }
 
 int main(void)
 {
-	int n = 10; //階乗する値
+	int normalSalary = 1072; //一般的な賃金体制
+
+	int recursiveSalary = 100; //再帰的な賃金体制の初期値
 	int result; //計算結果
 
-	result = Recursive(n);
-	printf("%dの階乗 = %d\n", n, result);
+	int workingTime = 9; //労働時間
 
+	//再帰的な体制の合計賃金を算出
+	result = Recursive(recursiveSalary,workingTime);
+	printf("再帰的体制の合計賃金 = %d\n\n",result);
+
+	//一般的な体制の合計賃金を算出
+	normalSalary *= workingTime;
+	printf("一般体制の合計賃金 = %d\n\n",normalSalary);
+
+	//二者を比較し、再帰的体制が上回ったかを確認
+	if (normalSalary < result) {
+		printf("再帰的な賃金体制の方が儲かったよ!\n");
+	}
+
+	//終了
 	return 0;
 }
