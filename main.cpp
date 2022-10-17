37080e285279ac1ef6b8536f97805ae0a2eddc83
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-17 10:49:01 +0900

    課題内容が修正される前の途中時点

--- main.cpp
+++ main.cpp
@@ -1,32 +1,58 @@
 #include <stdio.h>
-template <typename Type>
-Type Min(Type a, Type b) {
-	if (a < b) {
-		return a;
-	}
-	return b;
-}
+#include <string.h>
+#include <random>
 
-template <>
-char Min<char>(char a, char b) {
-	return 0;
-}
+void Input();
+bool NowWait(float time);
+int Return(char* str);
 
 int main(void)
 {
-	int a = 12;
-	int b = 34;
-	float c = 5.6f;
-	float d = 7.8f;
-	double e = 90.00001f;
-	double f = 23.00004f;
-	char g = '5';
-	char h = '8';
+	char str[] = "きすう";
+	int ans;
 
-	printf("%d\n", Min<int>(a, b));
-	printf("%1.2f\n", Min<float>(c, d));
-	printf("%1.5f\n", Min<double>(e, f));
-	printf("数字以外は代入できません\n", Min<char>(g,h));
-	
+	printf_s("入力 = %s\n\n", str);
+
+	ans = Return(str);
+
+	printf_s("\nans = %d\n", ans);
+
 	return 0;
+}
+
+bool NowWait(float time) {
+	return true;
+}
+
+int Return(char* str) {
+	const char ODD[] = "奇数";
+	const char EVEN[] = "偶数";
+	const char odd[] = "きすう";
+	const char even[] = "ぐうすう";
+
+	int ans = 0;
+
+	if (strcmp(str, ODD) == 0
+		|| strcmp(str, odd) == 0) {
+		ans = 1;
+
+		printf_s("【奇数の入力を確認】\n"
+			"%dが出力されました\n", ans);
+		return ans;
+	}
+
+	if (strcmp(str, EVEN) == 0
+		|| strcmp(str, even) == 0) {
+		ans = 2;
+
+		printf_s("【偶数の入力を確認】\n"
+			"%dが出力されました\n", ans);
+		return ans;
+	}
+
+	ans = 999;
+
+	printf_s("【想定外の入力を確認】\n"
+		"%dが出力されました\n", ans);
+	return ans;
 }
