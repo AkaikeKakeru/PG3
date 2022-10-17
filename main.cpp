d38a617693601361fa565d9a7f721d59a5e319db
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-17 19:38:36 +0900

    奇数か偶数かを入力して、的中したかどうかを返す

--- main.cpp
+++ main.cpp
@@ -2,57 +2,94 @@
 #include <string.h>
 #include <random>
 
-void Input();
+enum Number {
+	EvenNum = 0,
+	OddNum = 1,
+	ErrorNum = 999,
+};
+
+int InputPredict(char* str);
+
 bool NowWait(float time);
-int Return(char* str);
+bool Return(int input);
 
 int main(void)
 {
-	char str[] = "きすう";
-	int ans;
+	char str[10] = "きすう";
+	int input = 0;
+	bool judge = false;
+	int waitTimer = 3 * 50;
+
+
+	scanf_s("%s",&str,10);
+
+	input = InputPredict(str);
 
 	printf_s("入力 = %s\n\n", str);
 
-	ans = Return(str);
+	judge = Return(input);
 
-	printf_s("\nans = %d\n", ans);
+	printf_s("\nans = %d\n", judge);
 
 	return 0;
 }
 
-bool NowWait(float time) {
-	return true;
-}
-
-int Return(char* str) {
+int InputPredict(char* str) {
 	const char ODD[] = "奇数";
 	const char EVEN[] = "偶数";
 	const char odd[] = "きすう";
 	const char even[] = "ぐうすう";
 
-	int ans = 0;
+	int ans = EvenNum;
 
 	if (strcmp(str, ODD) == 0
 		|| strcmp(str, odd) == 0) {
-		ans = 1;
+		ans = OddNum;
 
-		printf_s("【奇数の入力を確認】\n"
-			"%dが出力されました\n", ans);
+		printf_s("【奇数の入力を確認】\n");
 		return ans;
 	}
 
 	if (strcmp(str, EVEN) == 0
 		|| strcmp(str, even) == 0) {
-		ans = 2;
+		ans = EvenNum;
 
-		printf_s("【偶数の入力を確認】\n"
-			"%dが出力されました\n", ans);
+		printf_s("【偶数の入力を確認】\n");
 		return ans;
 	}
 
-	ans = 999;
+	ans = ErrorNum;
 
 	printf_s("【想定外の入力を確認】\n"
 		"%dが出力されました\n", ans);
 	return ans;
+}
+
+bool NowWait(float time) {
+
+
+	return true;
+}
+
+bool Return(int input) {
+	//乱数シード生成器
+	std::random_device seed_gen;
+	//メルセンヌ・ツイスターの乱数エンジン
+	std::mt19937_64 engine(seed_gen());
+	//乱数範囲
+	std::uniform_real_distribution < float > dist(1, 10);
+
+	int ans = static_cast<int>(dist(engine));
+
+	printf_s("%dが選出されました\n", ans);
+
+	ans = ans % 2;
+
+	if (input == ans) {
+		printf_s("的中です\n");
+		return true;
+	}
+
+		printf_s("ハズレです\n");
+		return false;
 }
