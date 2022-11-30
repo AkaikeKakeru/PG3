783cc6cf570db7d28767c375dc9cd6f9ffb201d7
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-30 12:57:31 +0900

    list型コンテナで、for文による全要素アクセスをしてみる

--- main.cpp
+++ main.cpp
@@ -6,15 +6,30 @@
 
 using namespace std;
 
-int main(){
-	vector<string> f = {"a","b","c"};
-	int size = f.size();
+//int main(){
+//	vector<string> f = {"a","b","c"};
+//	int size = f.size();
+//
+//	for (vector<string>::iterator it_f = f.begin();
+//		it_f != f.end();
+//		it_f++){
+//
+//		cout << *it_f << endl;
+//	}
+//
+//	return 0;
+//}
 
-	for (vector<string>::iterator it_f = f.begin();
-		it_f != f.end();
+int main(){
+	list <int> lst{1,2,3,4,5};
+
+	int size = lst.size();
+
+	for (auto it_f = lst.begin();
+		it_f != lst.end();
 		it_f++){
 
-		cout << *it_f << endl;
+		cout << *it_f << "\n";
 	}
 
 	return 0;
