a13e19b9cf2bf262d7e714ee958895bd53c27674
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-30 11:24:02 +0900

    float型のvectorコンテナを作ってみる

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string>
 #include <vector>
 #include <list>
 
 using namespace std;
 
 int main(){
+	vector<float> f = {1.2,2.3,3.4};
+
+	int size = f.size();
+
+	for (int i = 0; i < size; i++){
+		printf_s("%f\n",f[i]);
+	}
 
 	return 0;
 }
