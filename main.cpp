12cfa0fe7aec4a99501f9d16f9c320ca87167643
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-02 11:21:38 +0900

    GetInsertCellAddress()を宣言定義

--- main.cpp
+++ main.cpp
@@ -7,10 +7,24 @@ typedef struct cell {
   struct cell *next;
 }CELL;
 
+CELL* GetInsertCellAddress(CELL* endCELL, int iteretor);
+
 int main(void) {
   SetConsoleOutputCP(CP_UTF8);
   setvbuf(stdout, nullptr, _IOFBF, 1024);
 
   system("pause");
   return 0;
+}
+
+CELL* GetInsertCellAddress(CELL* endCELL, int iteretor) {
+  for (int i = 0; i < iteretor; i++){
+    if (endCELL->next) {
+      endCELL = endCELL->next;
+    }
+    else {
+      break;
+    }
+  }
+  return endCELL;
 }
