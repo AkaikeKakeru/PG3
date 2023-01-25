0d52fbea475a019a24ed49b4da9bc2eaf437fcd9
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 16:42:23 +0900

    インスタンスを呼び出す

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <windows.h>
 #include <stdio.h>
+#include "IShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
 int main(void){
   SetConsoleOutputCP(CP_UTF8);
   setvbuf(stdout, nullptr, _IOFBF, 1024);
+
+  IShape* iShape[2];
+
+  for (size_t i = 0; i < 2; i++) {
+    if (i == 0) {
+      iShape[i] = new MyCircle();
+    }
+    else{
+      iShape[i] = new MyRectangle();
+    }
+  }
+
+  for (size_t i = 0; i < 2; i++) {
+    iShape[i]->Size();
+    iShape[i]->Draw();
+  }
+
+  for (size_t i = 0; i < 2; i++) {
+    delete iShape[i];
+  }
 
   system("pause");
   return 0;
