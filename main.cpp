02c42c4b2fc4f26d49b270605a0aedf51e53f364
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 16:03:35 +0900

    Enemyのインスタンスを生成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <windows.h>
 #include <stdio.h>
+#include "Enemy.h"
 
 int main(void){
   SetConsoleOutputCP(CP_UTF8);
   setvbuf(stdout, nullptr, _IOFBF, 1024);
 
+  Enemy* enemy = new Enemy;
+
+  while(true){
+    enemy->Update();
+
+    if (enemy->GetIsEnd()) {
+     return 0;
+    }
+  }
+
+  delete enemy;
   system("pause");
   return 0;
 }
