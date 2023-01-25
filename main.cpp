93cc1472025b55d3b8e291e1658dc788bc8b6297
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 13:10:45 +0900

    リストを、3種類同時に表示する仕様に変更

--- main.cpp
+++ main.cpp
@@ -4,15 +4,9 @@
 #include <list>
 #include <iostream>
 
+void DrawList(std::list <const char*> lst);
+
 int main() {
-  char input[8] = "1970";
-
-  char generation[3][8] = {
-    "1970",
-  "2019",
-  "2022"
-  };
-
   std::list <const char*> lst{
     "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
     "Uguisudani","Nippori","Tabata","Komagome",
@@ -24,44 +18,46 @@ int main() {
 
   int size = lst.size();
 
-  printf("1970、2019、2022、いずれかを入力してください。\n");
-  scanf_s("%s", input, 8);
+  //1970時点のリストを一覧表示
+  printf("表示年代 : 1970\n");
+  DrawList(lst);
 
-  //2019,または2022が入力されたら
-  if (strcmp(input, generation[1]) == 0
-    || strcmp(input, generation[2]) == 0) {
-    for (std::list<const char*>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {
-      //西日暮里を挿入追加
-      if (*itr == "Tabata") {
-        itr = lst.insert(itr, "Nishi-Nippori");
-        ++itr;
-      }
-
-      //2022の方が入力されていたら
-      if (strcmp(input, generation[2]) == 0) {
-        //高輪ゲートウェイを挿入追加
-        if (*itr == "Tamachi") {
-          itr = lst.insert(itr, "Takanawa Gateway");
-          ++itr;
-        }
-      }
+  //2019用のリストに修正
+  for (std::list<const char*>::iterator itr = lst.begin();
+    itr != lst.end(); ++itr) {
+    //西日暮里を挿入追加
+    if (*itr == "Tabata") {
+      itr = lst.insert(itr, "Nishi-Nippori");
+      ++itr;
     }
   }
 
-  //1970が入力されてなかったら
-  else if (strcmp(input, generation[0]) != 0) {
-    printf_s("\nERROR");
-    return 0;
+  //2019時点のリストを一覧表示
+  printf("\n表示年代 : 2019\n");
+  DrawList(lst);
+
+  //2022用のリストに修正
+  for (std::list<const char*>::iterator itr = lst.begin();
+    itr != lst.end(); ++itr) {
+    //高輪ゲートウェイを挿入追加
+    if (*itr == "Tamachi") {
+      itr = lst.insert(itr, "Takanawa Gateway");
+      ++itr;
+    }
   }
 
-  //リストを一覧表示
-  printf("\n表示年代 : %s\n", input);
+  //2022時点のリストを一覧表示
+  printf("\n表示年代 : 2022\n");
+  DrawList(lst);
+
+  system("pause");
+  return 0;
+}
+
+void DrawList(std::list <const char*> lst) {
   for (auto it_f = lst.begin();
     it_f != lst.end();
     it_f++) {
     std::cout << *it_f << "\n";
   }
-
-  system("pause");
-  return 0;
 }
