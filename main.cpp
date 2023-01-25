fe44a98b677d958437feca79211a30c0fa84544d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 13:29:31 +0900

    main.cppで生成したEnemyのインスタンス数を、静的変数enemyCount_で表示させる。

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <windows.h>
 #include <stdio.h>
+#include <iostream>
+#include "Enemy.h"
+
+int Enemy::enemyCount_;
 
 int main(void) {
   SetConsoleOutputCP(CP_UTF8);
   setvbuf(stdout, nullptr, _IOFBF, 1024);
 
+  Enemy* enemy1 = new Enemy;
+  Enemy* enemy2 = new Enemy;
+  Enemy* enemy3 = new Enemy;
+
+  std::cout << Enemy::enemyCount_ << std::endl;
+  delete enemy1;
+  std::cout << Enemy::enemyCount_ << std::endl;
+  delete enemy2;
+  delete enemy3;
+  std::cout << Enemy::enemyCount_ << std::endl;
+
   //ループ
   while (true) {
-
+    return 0;
   }
 
   system("pause");
