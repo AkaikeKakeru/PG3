4c181b2e15e36737e9164a5a4454e2824ed481a2
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 16:03:08 +0900

    Enemyの状態遷移を行う

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,29 @@
 #include "Enemy.h"
+#include <stdio.h>
+
+void (Enemy::* Enemy::spfuncTable[])()
+= {
+  &Enemy::Approach,
+  &Enemy::Fire,
+  &Enemy::Secession
+};
+
+void Enemy::Update() {
+  //現在シーンを呼び出す
+  (this->*spfuncTable[phase_])();
+}
+
+void Enemy::Approach(){
+  printf_s("敵は接近してきた\n");
+  SetPhase(firePhase);
+}
+
+void Enemy::Fire(){
+  printf_s("敵は射撃してきた\n");
+  SetPhase(secessionPhase);
+}
+
+void Enemy::Secession(){
+  printf_s("敵は離脱していった\n");
+  isEnd_ = true;
+}
--- Enemy.h
+++ Enemy.h
@@ -1,5 +1,27 @@
 #pragma once
 
 class Enemy {
+public:
+  enum PhaseEnum {
+    approachPhase,//接近
+    firePhase,//射撃
+    secessionPhase//離脱
+  };
 
+public:
+  void Update();
+
+  //フェーズセット
+  void SetPhase(size_t phase) { phase_ = phase; }
+  //終了取得
+  bool GetIsEnd() const { return isEnd_; }
+
+private:
+  size_t phase_ = 0;
+  bool isEnd_ = false;
+
+  static void (Enemy::* spfuncTable[])();
+  void Approach();//接近
+  void Fire();//射撃
+  void Secession();//離脱
 };
