36f306f3207da2c24ec4fb1dc9bb46447f5d9600
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-25 19:41:54 +0900

    静的生存boolがfalseなら全滅させる

--- main.cpp
+++ main.cpp
@@ -3,26 +3,50 @@
 #include <iostream>
 #include "Enemy.h"
 
+//静的 敵の数
 int Enemy::enemyCount_;
+//静的 敵の生存
+bool Enemy::isAlive_ = true;
 
 int main(void) {
   SetConsoleOutputCP(CP_UTF8);
   setvbuf(stdout, nullptr, _IOFBF, 1024);
 
-  Enemy* enemy1 = new Enemy;
-  Enemy* enemy2 = new Enemy;
-  Enemy* enemy3 = new Enemy;
+  //攻撃
+  bool attack = true;
 
-  std::cout << Enemy::enemyCount_ << std::endl;
-  delete enemy1;
-  std::cout << Enemy::enemyCount_ << std::endl;
-  delete enemy2;
-  delete enemy3;
-  std::cout << Enemy::enemyCount_ << std::endl;
+  //敵の配列
+  Enemy* enemys[5];
 
-  //ループ
-  while (true) {
-    return 0;
+  //インスタンス
+  for (size_t i = 0; i < _countof(enemys); i++) {
+    enemys[i] = new Enemy;
+  }
+  printf_s("敵の群れが現れた\n");
+  std::cout << Enemy::enemyCount_ << std::endl;
+  printf_s("\n");
+
+  //攻撃boolがtrueなら
+  if (attack) {
+    //1体、生存boolをfalseに
+    printf_s("敵を一体撃破\n\n");
+    enemys[3]->isAlive_ = false;
+  }
+
+  //静的 生存boolがfalseなら
+  if (Enemy::isAlive_ == false) {
+    for (size_t i = 0; i < _countof(enemys); i++) {
+      delete enemys[i];
+    }
+    printf_s("残り\n");
+    std::cout << Enemy::enemyCount_ << std::endl;
+  }
+  else {
+    printf_s("残り\n");
+    std::cout << Enemy::enemyCount_ << std::endl;
+    for (size_t i = 0; i < _countof(enemys); i++) {
+      delete enemys[i];
+    }
   }
 
   system("pause");
